61f0fc55dd88462a3efc0c960f3b14d09457c2da
r.guzhev <r.guzhev@bsl.dev>
2022-04-25 12:48:34 +0500

    dd

--- routes/index.js
+++ routes/index.js
@@ -14,6 +14,7 @@ router.get('/', async (ctx) => {
 
 router.get('/get-simple-upload-url', async (ctx) => {
     try {
+        console.log('ctx.query.fileName', ctx.query.fileName);
         const url = await s3Model.getSimpleUploadUrl(ctx.query.fileName)
         ctx.body = { url }
     } catch (error) {
